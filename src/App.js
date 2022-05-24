72097e22b456a3c23872af887b161ca1926356aa
Gitty Reisner <gitty@phone.do>
2022-05-24 23:15:45 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './login/login';
+import Chat from './chat/chat'
+import { Route, BrowserRouter, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login/>}></Route>
+          <Route exact path="/chat" element={<Chat/>}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/login/login.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import './login.css'
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+
+    const [userInformation, setUserInformation] = useState({
+        email: '',
+        userName: '',
+    })
+    const [isSucsses, setIsSucsses] = useState(true);
+
+    const navigate = useNavigate();
+
+    function handleChange(event) {
+        const value = event.target.value;
+        const name = event.target.name;
+        setUserInformation({
+            ...userInformation,
+            [name]: value
+        })
+    }
+
+    async function handleSubmit() {
+        await checkLoginStatus();
+    }
+
+    function checkLoginStatus() {
+        sessionStorage.setItem("isAuthenticated", false)
+        fetch(`http://localhost:3001/login?email=${userInformation.email}&userName=${userInformation.userName}`)
+            .then(response => response.json())
+            .then(res => {
+                if (res.sucsses) {
+                    sessionStorage.setItem("isAuthenticated", true);
+                    navigate('/chat', { state : { userInformation } })
+                }
+                else {
+                    setUserInformation({
+                        email: '',
+                        userName: '',
+                    });
+                }
+                setIsSucsses(res.sucsses);
+            })
+    }
+
+    return (
+        <div>
+            <main className="join-main">
+                <div className="form-control">
+                    <label >Email Address</label>
+                    <input
+                        value={userInformation.email}
+                        onChange={handleChange}
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Enter email address..."
+                        required
+                    />
+                </div>
+                <div className="form-control">
+                    <label>Username</label>
+                    <input
+                        value={userInformation.userName}
+                        onChange={handleChange}
+                        type="text"
+                        name="userName"
+                        id="username"
+                        placeholder="Enter username..."
+                        required
+                    />
+                </div>
+                {!isSucsses &&
+                    <div className="errMsg">User does not exist please register</div>}
+                <div className="flex">
+                    <button type="submit" className="btn" onClick={handleSubmit}>Login to Chat</button>
+                </div>
+            </main>
+        </div>
+    );
+}
+export default Login;
